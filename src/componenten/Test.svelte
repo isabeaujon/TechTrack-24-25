72897bd1703a6099c08d0a1be7d89bfc6d918409
isabeaujon <isa.beaujon@hva.nl>
2023-11-29 16:23:13 +0100

    Initial commit

---
+++ src/componenten/Test.svelte
@@ -0,0 +1,15 @@
+<script>
+ import { onMount } from 'svelte';
+ import * as d3 from 'd3';
+
+ onMount(async () => {
+//javascript code komt hier tussen
+ });
+</script>
+
+<p>
+Hello world
+</p>
+
+<style>
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Test from "../componenten/Test.svelte";
+</script>
+
+<main>
+    <Test/>
+</main>
